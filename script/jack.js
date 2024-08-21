4add4687709635cbb421539218beb3da506c37b9
DexterBakr <bakrmostafa20@gmail.com>
2024-08-21 20:44:01 +0300

    depi

---
+++ script/jack.js
@@ -0,0 +1,8 @@
+window.alert('hello world'); 
+document.write("<h2>Hello World!</h2><p>Have a nice day!</p>");
+console.log("first");
+console.log(["'hi',25,'bue',652,323"]);
+console.log(typeof {name:'ahmed',age:3265});
+console.error("error message");
+console.table(['jack',22,'mark',232,'mr fox',1050]);
+console.log("Mostafa %c Bakr" , "color:red;  font-size:50px;");
